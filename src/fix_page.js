4aa7ae1236bfb99b7cf02c82b5f5b1e64ffc28bb
deevroman <roman.deev06@gmail.com>
2020-03-14 18:19:53 +0300

    bug fixes

--- src/fix_page.js
+++ src/fix_page.js
@@ -1,6 +1,10 @@
 document.querySelectorAll('a').forEach(function (elem) {
-    let reg = /^((https:\/\/|http:\/\/)?(telegram|t)\.me\/([a-zA-z0-9_]{1,}))/g;
-    elem.href = elem.href.replace(reg, "tg://resolve?domain=$4");
-    // reg = /^((https:\/\/|http:\/\/)?(telegram|t)\.me\/joinChat\/([a-zA-z0-9_]{1,}))/g;
-    // elem.href = elem.href.replace(reg, "tg://resolve?invite=$4");
+    let reg = /^((https:\/\/|http:\/\/)?(telegram|t)\.me\/([a-zA-z0-9_]{1,}))$/g;
+    if (reg.test(elem.href)) {
+        elem.href = elem.href.replace(reg, "tg://resolve?domain=$4");
+    }
+    reg = /^((https:\/\/|http:\/\/)?(telegram|t)\.me\/joinchat\/([a-zA-z0-9_]{1,}))$/g;
+    if (reg.test(elem.href)) {
+        elem.href = elem.href.replace(reg, "tg://resolve?invite=$4");
+    }
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,7 +4,7 @@ function fixUrl(event) {
         let fixedLink = event.target.value.replace(reg, "tg://resolve?domain=$4");
         document.getElementById("output-link").innerHTML = "<a href='" + fixedLink + "'>" + fixedLink + "</a>";
     } else {
-        let reg = /^((https:\/\/|http:\/\/)?(telegram|t)\.me\/joinchat\/([a-zA-z0-9_]{1,}))/g;
+        let reg = /^((https:\/\/|http:\/\/)?(telegram|t)\.me\/joinchat\/([a-zA-z0-9_]{1,}))$/g;
         if (reg.test(event.target.value)) {
             let fixedLink = event.target.value.replace(reg, "tg://resolve?invite=$4");
             document.getElementById("output-link").innerHTML = "<a href='" + fixedLink + "'>" + fixedLink + "</a>";
