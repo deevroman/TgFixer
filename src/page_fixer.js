453610869a63de1620778887dcb2a6a10da3cf65
deevroman <roman.deev06@gmail.com>
2021-07-24 22:17:24 +0300

    bug fix

--- src/page_fixer.js
+++ src/page_fixer.js
@@ -1,4 +1,4 @@
-document.querySelectorAll("a[href*='t.me'], a[href*='telegram.org'], a[href*='telegram.dog']").forEach(function (elem) {
+document.querySelectorAll("a[href*='t.me'], a[href*='telegram.me'], a[href*='telegram.dog']").forEach(function (elem) {
     let fixed_url = fixUrl(elem.href);
     if (fixed_url) {
         elem.href = fixed_url;
